9cb88eca8579d6a48dc4103b3930fbf3d5f870da
Aru Gyani <gyaniaru@gmail.com>
2022-01-09 23:35:12 -0600

    initial commit

--- dist/bot.js
+++ dist/bot.js
@@ -1,41 +1,33 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const discord_js_1 = require("discord.js");
 const fs = require('fs');
 const { Client, Intents } = require('discord.js');
-const { token } = require('../config.json');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { token, clientId, guildId } = require('../config.json');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 client.commands = new discord_js_1.Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commands = [];
+const commandFiles = fs.readdirSync('./dist/commands').filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync('./dist/events').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.data.name, command);
+    commands.push(command.data.toJSON());
 }
-client.on('ready', () => {
-    console.log('Ready...');
-});
-client.on('interactionCreate', (interaction) => __awaiter(void 0, void 0, void 0, function* () {
-    if (!interaction.isCommand())
-        return;
-    const command = client.commands.get(interaction.commandName);
-    if (!command)
-        return;
-    try {
-        yield command.execute(interaction);
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
     }
-    catch (error) {
-        console.error(error);
-        yield interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    else {
+        client.on(event.name, (...args) => event.execute(...args));
     }
-}));
+}
+const rest = new REST({ version: '9' }).setToken(token);
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+    .then(() => console.log('Successfully registered application commands.'))
+    .catch(console.error);
 client.login(token);
 //# sourceMappingURL=bot.js.map
--- src/bot.ts
+++ src/bot.ts
@@ -2,37 +2,39 @@ import { Collection } from "discord.js";
 
 const fs = require('fs');
 const { Client, Intents } = require('discord.js');
-const { token } = require('../config.json');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { token, clientId, guildId } = require('../config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.commands = new Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commands = [];
+
+const commandFiles = fs.readdirSync('./dist/commands').filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync('./dist/events').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
 
     client.commands.set(command.data.name, command);
+    commands.push(command.data.toJSON());
 }
 
-client.on('ready', () => {
-    console.log('Ready...');
-});
-
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
-
-	const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
 	}
-});
+}
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
 
 client.login(token);
