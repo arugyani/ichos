8c30d97b47936a6b64748e6ce8b286430bc9ad04
Eralp Orkun <eaorkun@gmail.com>
2022-01-11 17:57:53 -0600

    removed config file and added resume command

---
+++ src/commands/resume.ts
@@ -0,0 +1,26 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { joinVoiceChannel } from '@discordjs/voice';
+import { GuildMember } from 'discord.js';
+
+import { MusicSubscription } from '../music/subscription';
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('resume')
+		.setDescription('Resumes play for ichos.'),
+	async execute(interaction) {
+        const subscriptions = require('../bot');
+		let subscription = subscriptions.get(interaction.guildId);
+
+        if (!subscription) { // no subscription exists
+            await interaction.reply('ichos is not in a voice channel');
+        } else { // connection exists
+    
+            if (subscription) {
+                subscription.player.unpause();
+    
+                await interaction.reply('Resuming play!');
+            } else await interaction.reply('No track is currently paused!');
+        }
+	},
+};
